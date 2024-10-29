69a9358de73a1c2c1124d27a68c09850748efefe
crist <crist@192.168.7.86>
2024-10-29 13:13:13 +0100

    commit inicial

---
+++ M05_practica_GIT/src/main/java/m05_practica_git/M05_practica_GIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package m05_practica_git;
+
+/**
+ *
+ * @author crist
+ */
+public class M05_practica_GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
